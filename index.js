8f96d68b6490792bb1eaa4ee632cbdb26692e044
Rizal <rizalnug2@gmail.com>
2024-12-23 16:49:43 +0700

    route utk menentukan response

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const res = require('express/lib/response');
 
 const app = express();
 
-app.use((req, res)=>{
-    console.log('Get Request');
-    res.send({
-        name: 'Rizal',});
+// app.use((req, res)=>{
+//     console.log('Get Request');
+//     res.send({
+//         name: 'Rizal',});
+// })
+
+app.get('/', (req, res) => {
+    res.send('This is Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is About Page');
+}); 
+
+app.get('/product', (req, res) => {
+    res.send('This is Product Page');
+});
+
+app.post('/product', (req, res) => {
+    res.send('This is Product Page from POST');
+});
+
+app.get('*', (req, res) => {
+    res.send('404 Page Not Found');
 })
 
 app.listen(8080, () => {
